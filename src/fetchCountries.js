015153f35b7869fbd9471d392a205c014c46821d
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-28 14:17:10 +0200

    add fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,11 @@
 export const fetchCountries = function (name) {
-  console.log('GOOD!');
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+  return fetch(`${BASE_URL}${name}`).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    return resp.json();
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import './css/styles.css';
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
+
+import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-fetchCountries();
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  fetchCountries(input.value)
+    .then(data => console.log(data))
+    .catch();
+}
+
+// { countries => {
+//   countries.map({
+//     name: { official },
+//     capital,
+//     population,
+//     flags: { svg: flagSvg },
+//     languages,
+//   });
+// };}
