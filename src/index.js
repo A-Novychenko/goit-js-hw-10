70bcd231bd5acd604f8e9558f8c92f1f75bbe67b
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-28 21:06:34 +0200

    pre-compl

--- src/index.js
+++ src/index.js
@@ -11,34 +11,43 @@ const list = document.querySelector('.country-list');
 const card = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+document.body.style.backgroundImage = `url(https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1920px-Flag-map_of_the_world.svg.png)`;
 
 function onSearch(e) {
   const inputName = input.value.trim();
   if (!inputName) {
     list.innerHTML = '';
+    document.body.style.backgroundImage = `url(https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1920px-Flag-map_of_the_world.svg.png)`;
 
     return;
   }
 
   fetchCountries(inputName)
     .then(coutries => {
+      document.body.style.backgroundImage = `url(https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Flag-map_of_the_world.svg/1920px-Flag-map_of_the_world.svg.png)`;
+
       if (coutries.length > 10) {
         card.innerHTML = '';
         list.innerHTML = '';
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
+
         return;
       } else if (coutries.length > 1 && coutries.length <= 10) {
         card.innerHTML = '';
+        // document.body.style.backgroundImage = `none`;
+
         createListCoutriesMurkup(coutries);
       } else {
-        createListCoutriesMurkup(coutries);
+        list.innerHTML = '';
         createCardCoutryMurkup(coutries);
       }
     })
     .catch(() => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      list.innerHTML = '';
+      document.body.style.backgroundImage = `url(https://img.freepik.com/free-vector/oops-404-error-with-a-broken-robot-concept-illustration_114360-5529.jpg?w=826&t=st=1672246439~exp=1672247039~hmac=40e8c5bf1f5e1f11c7b8a188661ba7ce89c1d7a27681fa5bed3612be9d1eb9e6)`;
     });
 }
 
@@ -64,12 +73,16 @@ function createCardCoutryMurkup(coutries) {
     }) => {
       const langList = Object.values(languages);
 
+      document.body.style.backgroundImage = `url(${flagSvg})`;
+
       return `
-     <img src="${flagSvg}" alt="${official}" width="100">
-     <p>${official}</p>
-    <p>${capital}</p>
-    <p>${population}</p>
-    <p>${langList}</p>`;
+     <img src="${flagSvg}" alt="${official}" width="220">
+     <div class="wrap">
+        <h1>${official}</h1>
+        <p>Capital: ${capital}</p>
+        <p>Population: ${population}</p>
+        <p>Languages: ${langList}</p>
+     </div>`;
     }
   );
 
