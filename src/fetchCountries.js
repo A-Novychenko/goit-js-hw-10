c48fde5febd224f4cf77cfcef27c6afb5cd20df6
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-28 15:36:02 +0200

    add handle then

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,9 @@
 export const fetchCountries = function (name) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-  return fetch(`${BASE_URL}${name}`).then(resp => {
+  return fetch(
+    `${BASE_URL}${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
@@ -9,3 +11,9 @@ export const fetchCountries = function (name) {
     return resp.json();
   });
 };
+
+//     name: { official },
+//     capital,
+//     population,
+//     flags: { svg: flagSvg },
+//     languages,
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const card = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
-  fetchCountries(input.value)
-    .then(data => console.log(data))
+  const inputName = input.value.trim();
+  if (!inputName) {
+    console.log('ПУСТОЙ');
+    list.innerHTML = '';
+
+    return;
+  }
+  console.log('НЕ ПУСТОЙ');
+  fetchCountries(inputName)
+    .then(coutries => {
+      console.log(coutries);
+      if (coutries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      } else if (coutries.length > 1 && coutries.length < 10) {
+        createListCoutriesMurkup(coutries);
+      } else {
+        createListCoutriesMurkup(coutries);
+        createCardCoutryMurkup(coutries);
+      }
+    })
     .catch();
 }
 
@@ -24,3 +48,24 @@ function onSearch(e) {
 //     languages,
 //   });
 // };}
+
+function createListCoutriesMurkup(coutries) {
+  const markup = coutries.map(
+    ({ name: { official }, flags: { svg: flagSvg } }) => `<li>
+  <img src="${flagSvg}" alt="${official}" width="100">
+  <p>${official}</p>
+</li>`
+  );
+  list.innerHTML = markup.join(' ');
+}
+
+function createCardCoutryMurkup(coutries) {
+  const markup = coutries.map(({ capital, population, languages }) => {
+    const langList = Object.values(languages);
+
+    return `<p>${capital}</p>
+  <p>${population}</p>
+  <p>${langList}</p>`;
+  });
+  card.innerHTML = markup.join(' ');
+}
